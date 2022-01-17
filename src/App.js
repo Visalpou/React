e71e205a041cb3886e266c17a19db476c4ca0d2a
Visalpou <povvisal001@gmail.com>
2022-01-17 15:30:47 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Product from './Product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+  const [advice, setAdvice] = useState("")
+    useEffect(() => {
+      const url = "https://api.adviceslip.com/advice"
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          console.log(json);
+          setAdvice(json.slip.advice);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+  
+      fetchData();  
+    }, []);
+
+      
+    return (
+      <div>
+          <h1>{advice}</h1>
+          <Product />
+      </div>
+    );
+    
+};
 
 export default App;
